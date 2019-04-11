4d179931eb365492c86ef8173ef9159295d0babd
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-11 17:14:56 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara/rspec' # NOTE: I DON'T have this but you do...
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 RSpec.configure do |config|
-
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
 end
